ab7edf95bda4c3043504f6ff50010ca4bf719c5a
Coding-With-Tanvirsiraj <162613551+Coding-With-Tanvirsiraj@users.noreply.github.com>
2024-03-20 21:01:48 +0500

    Add files via upload

---
+++ 10.Adding Comments P1.ts
@@ -0,0 +1,7 @@
+// This program is written by: Tanvir siraj
+
+// Current Date is: 20-3-2024
+
+// This page will write: Hellow World
+
+console.log( 'Hellow World' );
---
+++ 10.Adding Comments P2.ts
@@ -0,0 +1,8 @@
+// This program is written by: Tanvir siraj
+
+// Current Date is: 20-3-2024
+
+// This page will add two digits
+
+console.log(2+4);
+
